aac68c637865ab793c1516bf48d92d8891ebbaa5
Kathryn Sutton <kathrynsutton@Kathryns-MacBook-Pro.local>
2015-09-15 23:26:47 -0400

    getting better

--- CollisionsAnaRunner.java
+++ CollisionsAnaRunner.java
@@ -6,8 +6,19 @@
  */
 public class Collision {
 
+	//zipcode where it occurred
+	int zip;
 	
-		
-
-
+	//number total persons injuries and fatalities
+	int injured;
+	
+	//number of cyclist injuries and fatalities
+	int cyc_injuried;
+	
+	//vehicle type
+	String vehicle;
+	
+	//default constructor
+	Collision(){
+	}
 }
